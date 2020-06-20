b4af1bd4fb69c762427805da6f7a7e4e41e3876a
FajarJunianto16 <55769396+FajarJunianto16@users.noreply.github.com>
2020-06-20 17:09:23 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,7 @@
+<?php
+    echo' HALLO GUYS <br>';
+    echo' Nama : Fajar Junianto <br>';
+    echo' Kelas : XI RPL 1 <br>';
+    echo' SMK TARUNA BHAKTI';
+    echo' No Telpon : 08155175535 <br>'; 
+?>
